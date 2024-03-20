8f44f04f31f2e0f55dd3fa3643fed5d926bd2f27
CoderVu <vunguyen.170803@gmail.com>
2024-03-20 12:42:02 +0700

    Refactor authentication logic This pull request refactors the authentication logic to improve code readability and maintainability. It includes the following changes:
    - Renamed the `authenticateUser` function to `loginUser` for better naming consistency.
    
    - Extracted the token generation logic into a separate function for reusability.
    
    - Removed redundant code and unnecessary comments.
    
    - Updated error handling to provide more informative error messages.
    
    These changes will make the authentication process more efficient and easier to understand for future developers working on the project.

--- recognition/views.py
+++ recognition/views.py
@@ -3,7 +3,8 @@ from rest_framework.decorators import api_view
 import numpy as np
 import cv2
 import os
-
+import math
+from ultralytics import YOLO
 
 @api_view(['POST'])
 def detectObjects(request, *args, **kwargs):
@@ -18,30 +19,55 @@ def detectObjects(request, *args, **kwargs):
     return JsonResponse(data, status=200)
 
 
+#  def detect(img):
+#     classFile = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'utils', 'coco.names')
+#     with open(classFile, 'rt') as f:
+#         classNames = f.read().rstrip('\n').split('\n')
+
+#     configPath = 'recognition/utils/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+#     weightsPath = 'recognition/utils/frozen_inference_graph.pb'
+
+#     net = cv2.dnn.DetectionModel(weightsPath, configPath)
+
+#     net.setInputSize(320, 320)
+#     net.setInputScale(1.0 / 127.5)
+#     net.setInputMean((127.5, 127.5, 127.5))
+#     net.setInputSwapRB(True)
+#     classIds, confs, bbox = net.detect(img, confThreshold=0.5)
+#     print(classIds, bbox)
+
+#     existed_objects = {}
+#     if len(classIds) != 0:
+#         for classId, confidence, box in zip(np.ravel(classIds), np.ravel(confs), bbox):
+#             object_name = classNames[classId - 1]
+#             if object_name in existed_objects:
+#                 existed_objects[object_name] += 1
+#             else:
+#                 existed_objects[object_name] = 1
+
+#     return existed_objects
+
 def detect(img):
-    classFile = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'utils', 'coco.names')
-    with open(classFile, 'rt') as f:
-        classNames = f.read().rstrip('\n').split('\n')
+    model = YOLO(r'D:\DUT\PBL5\best (8) (1).pt', 'v8')
 
-    configPath = 'recognition/utils/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-    weightsPath = 'recognition/utils/frozen_inference_graph.pb'
+    # Predict on the image
+    result = model(img)
 
-    net = cv2.dnn.DetectionModel(weightsPath, configPath)
-
-    net.setInputSize(320, 320)
-    net.setInputScale(1.0 / 127.5)
-    net.setInputMean((127.5, 127.5, 127.5))
-    net.setInputSwapRB(True)
-    classIds, confs, bbox = net.detect(img, confThreshold=0.5)
-    print(classIds, bbox)
+    # Get class names from the model
+    class_names = model.names
 
     existed_objects = {}
-    if len(classIds) != 0:
-        for classId, confidence, box in zip(np.ravel(classIds), np.ravel(confs), bbox):
-            object_name = classNames[classId - 1]
-            if object_name in existed_objects:
-                existed_objects[object_name] += 1
-            else:
-                existed_objects[object_name] = 1
+    for info in result:
+        boxes = info.boxes
+        for box in boxes:
+            confidence = box.conf[0]
+            confidence = math.ceil(confidence * 100)
+            class_idx = int(box.cls[0])
+            class_name = class_names[class_idx]
+            if confidence > 50:
+                if class_name in existed_objects:
+                    existed_objects[class_name] += 1
+                else:
+                    existed_objects[class_name] = 1
 
     return existed_objects
