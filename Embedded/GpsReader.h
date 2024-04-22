fe69350930ab5118263266b09b4c55c68f0c84e7
phuocduy <phuocduylpd108@gmail.com>
2024-04-22 21:19:44 +0700

    feat class GpsReader

---
+++ Embedded/GpsReader.h
@@ -0,0 +1,22 @@
+#include <TinyGPSPlus.h>
+#include <SoftwareSerial.h> 
+class GpsReader{
+  private:
+    TinyGPSPlus gps; 
+    SoftwareSerial gpsSerial;
+  public:
+    GpsReader(int RX, int TX): gpsSerial(RX,TX){
+      gpsSerial.begin(9600);  
+    } 
+    bool getLocation(double &_lat, double &_long){
+      while (gpsSerial.available()>0) {
+        gps.encode(gpsSerial.read());  
+      }
+      if(gps.location.isUpdated()){
+          _lat = gps.location.lat();
+          _long = gps.location.lng();
+          return true;
+      }
+      return false;
+    } 
+};
