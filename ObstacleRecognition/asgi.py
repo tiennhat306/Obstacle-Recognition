74ad60cfa968adde6c19107ce541391280fc0312
tien.nhat <tiennhat306@gmail.com>
2024-06-05 19:16:08 +0700

    update(BE): websocket detection for ESP32CAM

--- ObstacleRecognition/asgi.py
+++ ObstacleRecognition/asgi.py
@@ -8,22 +8,22 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import path
 import recognition.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ObstacleRecognition.settings')
 
-# application = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            recognition.routing.websocket_urlpatterns
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                recognition.routing.websocket_urlpatterns
+            )
         )
     ),
 })
