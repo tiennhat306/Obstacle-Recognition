1fd5c19da624868ccb715763053e1f14a6af66f8
CoderVu <vunguyen.170803@gmail.com>
2024-06-10 08:48:22 +0700

    final model

--- recognition/models.py
+++ recognition/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
 from ultralytics import YOLO
-YOLOv8_model = YOLO('recognition/utils/YOLOv8_best.pt', 'v8')
+YOLOv8_model = YOLO('recognition/utils/best_200_epoch.pt', 'v8')
